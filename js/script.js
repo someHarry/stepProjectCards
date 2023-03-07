50e2cdbe251846a97a55ad7f6cf3eb973052b6ba
KarinaIvaschenko <karinka2323@gmail.com>
2023-03-08 01:42:24 +0200

    added modal

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 import  Modal  from "./Modal.js";
 
 const modalTemplate = document.querySelector('#modal').content;
-let mymodal = new Modal(modalTemplate);
-document.querySelector("body").append(mymodal.render())
-console.log(mymodal.render())
+
+let mymodal = new Modal(modalTemplate, 'ewgrg','eg3rgrg');
+
+document.querySelector("body").append(mymodal.render());
+console.log(mymodal.render());
