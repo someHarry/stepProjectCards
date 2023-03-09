582a064a7b2e3dc5482406356f27a7784f93146c
KarinaIvaschenko <karinka2323@gmail.com>
2023-03-09 15:22:25 +0200

    form

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
 import {buttonLogin, buttonCreateCard} from "./constants.js";
 
 import  Modal  from "./Modal.js";
+import Form from "./Form.js";
 
 const modalTemplate = document.querySelector('#modal').content;
 
-let mymodal = new Modal(modalTemplate, 'ewgrg','eg3rgrg');
+const form = new Form('knopka', 'con', function (){
+    console.log(this)
+})
+
+let mymodal = new Modal(modalTemplate, 'ewgrg',form.render(), 'modal-window');
 
 document.querySelector("body").append(mymodal.render());
 console.log(mymodal.render());
+
+
 
 
 
