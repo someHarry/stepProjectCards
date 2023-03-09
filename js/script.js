70a89e2f1e2b41460a4ea3df84822b3241222c4f
unknown <kiparisborismoris@gmail.com>
2023-03-09 20:52:12 +0200

    Visit, first try

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
-import {buttonLogin, buttonCreateCard, loginUrl} from "./constants.js";
-import {token, isToken, loginHandler, TEMPORARYcredentials} from "./Login.js";
+import { buttonLogin, buttonCreateCard, loginUrl } from "./constants.js";
+import { token, isToken, loginHandler, TEMPORARYcredentials } from "./Login.js";
 
-import  Modal  from "./Modal.js";
+import Modal from "./Modal.js";
 import Form from "./Form.js";
 import FormLogin from "./FormLogin.js";
 
-const modalTemplate = document.querySelector('#modal').content;
+const modalTemplate = document.querySelector("#modal").content;
 
-const form = new Form('knopka', function (){
-    // console.log(this)
-})
+const form = new Form("knopka", function () {
+  // console.log(this)
+});
 
-const formLogin = new FormLogin('knopka', function (){
-    // console.log(this)
-})
+const formLogin = new FormLogin("knopka", function () {
+  // console.log(this)
+});
 
-
-let mymodal = new Modal(modalTemplate, 'ewgrg', form.render(formLogin.renderInput()), 'modal-window');
+let mymodal = new Modal(
+  modalTemplate,
+  "ewgrg",
+  form.render(formLogin.renderInput()),
+  "modal-window"
+);
 
 document.querySelector("body").append(mymodal.render());
-
-
-
-
