864c4b0f77b18ef43c40a40545ee510d68446182
KarinaIvaschenko <karinka2323@gmail.com>
2023-03-09 21:04:25 +0200

    formVisit

--- js/FormVisit.js
+++ js/FormVisit.js
@@ -25,4 +25,35 @@ export default class FormVisit extends Form {
         select.append(optionSelected,optionCardiologist,optionDentist,optionTherapist);
         return select;
     }
+    renderFields(){
+        // <label for="family-name-field">Фамилия</label>
+        // <input id="family-name-field" type="text" name="family-name">
+        // цель визита
+        // краткое описание визита
+        // выпадающее поле - срочность (обычная, приоритетная, неотложная)
+        // ФИО
+        const labelVisit = document.createElement('label');
+        labelVisit.htmlFor = "label-visit";
+        labelVisit.textContent = "Цель визита";
+        const inputVisit = document.createElement('input');
+        inputVisit.id = "label-visit";
+        inputVisit.type = "text";
+        inputVisit.placeholder = "Цель визита";
+        const labelDescription = document.createElement('label');
+        labelDescription.htmlFor = "description-visit";
+        labelDescription.textContent = "Краткое описание визита";
+        const inputDescription = document.createElement('input');
+        inputDescription.id = "description-visit";
+        inputDescription.type = "text";
+        inputDescription.placeholder = "Описание визита";
+
+
+        const labelName = document.createElement('label');
+        labelName.htmlFor = "description-visit";
+        labelName.textContent = "Краткое описание визита";
+        const inputName = document.createElement('input');
+        inputName.id = "description-visit";
+        inputName.type = "text";
+        inputName.placeholder = "Описание визита";
+    }
 }
