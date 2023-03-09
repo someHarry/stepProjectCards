56729ffda8aced52007faf3a485cbc57dd0968af
HarrySome <21parit@gmail.com>
2023-03-09 18:15:59 +0200

    loginHandler

--- js/Login.js
+++ js/Login.js
@@ -1,6 +1,6 @@
 let token = localStorage.getItem('token'); // Получаем token из local storage
 
-function checkToken (){
+function isToken (){
     if (!!token) {       // Проверяем, получили ли мы токен, если токен не null, прячем форму авторизации и показываем кнопку "Создать карточку"
         // hide(loginButton);      // Надо будет создать функцию, которая навешивает класс hidden
         // show(createCardButton); // ...и убирает его
@@ -9,14 +9,16 @@ function checkToken (){
 
 
 // Якобы у нас есть форма, юзер ввёл туда логин-пароль, нажал "отправить" и у нас есть эти данные. Пока что просто переменная:
-const credentials = {email:"your@email.com",password:"password"};
+const TEMPORARYcredentials = {email:"your@email.com",password:"password"};
 
 function loginHandler(credentials){
-    Requests.POST(loginUrl, credentials).then(tk => {    // Типа обработка события click на кнопке отправки
-        token = tk;
-        localStorage.setItem('token', token);
-    })
+    Requests.POST(loginUrl, credentials)    // Типа обработка события click на кнопке отправки
+        .then(tk => {
+            token = tk;
+            localStorage.setItem('token', token);
+        })
+        .then(isToken)
 }
 
 
-export {token, checkToken, loginHandler}
+export {token, isToken, loginHandler, TEMPORARYcredentials}
