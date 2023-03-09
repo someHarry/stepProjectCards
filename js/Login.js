79bba74e0d023c3f4f8cf74b7b5d311bff5b1e9b
HarrySome <21parit@gmail.com>
2023-03-09 17:22:43 +0200

    a little of everything

--- js/Login.js
+++ js/Login.js
@@ -1,15 +1,22 @@
-const loginUrl = "https://ajax.test-danit.com/api/v2/cards/login"; // будет перенесено в файл с константами
-
 let token = localStorage.getItem('token'); // Получаем token из local storage
-if (!!token) {       // Проверяем, что получили
-    // Если токен не null, прячем форму авторизации и показываем кнопку "Создать карточку"
-    hide(loginButton);      // Надо будет создать функцию, которая навешивает класс hidden
-    show(createCardButton); // ...и убирает его
+
+function checkToken (){
+    if (!!token) {       // Проверяем, получили ли мы токен, если токен не null, прячем форму авторизации и показываем кнопку "Создать карточку"
+        // hide(loginButton);      // Надо будет создать функцию, которая навешивает класс hidden
+        // show(createCardButton); // ...и убирает его
+    }
 }
+
 
 // Якобы у нас есть форма, юзер ввёл туда логин-пароль, нажал "отправить" и у нас есть эти данные. Пока что просто переменная:
 const credentials = {email:"your@email.com",password:"password"};
-Requests.POST(loginUrl, credentials).then(tk => {    // Типа обработка события click на кнопке отправки
-    token = tk;
-    localStorage.setItem('token', token);
-})
+
+function loginHandler(credentials){
+    Requests.POST(loginUrl, credentials).then(tk => {    // Типа обработка события click на кнопке отправки
+        token = tk;
+        localStorage.setItem('token', token);
+    })
+}
+
+
+export {token, checkToken, loginHandler}
--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-import {buttonLogin, buttonCreateCard} from "./constants.js";
+import {buttonLogin, buttonCreateCard, loginUrl} from "./constants.js";
+
+import  Modal  from "./Modal.js";
+
+const modalTemplate = document.querySelector('#modal').content;
+
+let mymodal = new Modal(modalTemplate, 'ewgrg','eg3rgrg');
+
+document.querySelector("body").append(mymodal.render());
+console.log(mymodal.render());
