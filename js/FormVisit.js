2c3cbb58c898ce0e6f1a9481d55e6e3088727cf6
KarinaIvaschenko <karinka2323@gmail.com>
2023-03-09 19:21:05 +0200

    formLogin

---
+++ js/FormVisit.js
@@ -0,0 +1,9 @@
+import Form from "./Form.js";
+export default class FormVisit extends Form{
+    constructor(submitTitle , formHandler) {
+        super(submitTitle , formHandler);
+    }
+    renderVisit(){
+
+    }
+}
