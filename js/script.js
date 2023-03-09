4c362ae761be9a5e743fb8f9cb726b3cc3254d90
KarinaIvaschenko <karinka2323@gmail.com>
2023-03-09 18:22:22 +0200

    added login

--- js/script.js
+++ js/script.js
@@ -7,18 +7,18 @@ import FormLogin from "./FormLogin.js";
 const modalTemplate = document.querySelector('#modal').content;
 
 const form = new Form('knopka', function (){
-    console.log(this)
+    // console.log(this)
 })
 
 const formLogin = new FormLogin('knopka', function (){
-    console.log(this)
+    // console.log(this)
 })
 
 
 let mymodal = new Modal(modalTemplate, 'ewgrg', formLogin.renderInput(), 'modal-window');
 
 document.querySelector("body").append(mymodal.render());
-console.log(mymodal.render());
+
 
 
 
