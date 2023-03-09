628a2e501342562ea461f9f0f37a7a1675c690b3
HarrySome <21parit@gmail.com>
2023-03-09 21:41:08 +0200

    formLogin.render(formLogin.renderInput())

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ const formVisit = new FormVisit('hello', function (){
     console.log(this)
 })
 
-let mymodal = new Modal(modalTemplate, 'ewgrg', formLogin.renderInput(), 'modal-window');
+let mymodal = new Modal(modalTemplate, 'ewgrg', formLogin.render(formLogin.renderInput()), 'modal-window');
 
 document.querySelector("body").append(mymodal.render());
 
