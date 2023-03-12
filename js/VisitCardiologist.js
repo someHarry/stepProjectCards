ca3b045649458e92ab06e0f5c738346ec0f8af01
unknown <kiparisborismoris@gmail.com>
2023-03-12 10:27:41 +0200

    visit children

--- js/VisitCardiologist.js
+++ js/VisitCardiologist.js
@@ -1,4 +1,4 @@
-import { Visit } from "./Visit.js";
+import { visitCard, Visit } from "./Visit.js";
 
 class VisitCardiolog extends Visit {
   constructor(
@@ -17,6 +17,29 @@ class VisitCardiolog extends Visit {
     this.bodyMass = bodyMass;
     this.prevDiseases = prevDiseases;
     this.age = age;
+  }
+
+  render() {
+    const age = document.createElement("p");
+    age.className = "visitCard__container--age";
+    age.textContent = this.age;
+
+    const bodyMass = document.createElement("p");
+    bodyMass.className = "visitCard__container--bodyMass";
+    bodyMass.textContent = this.bodyMass;
+
+    const normPressure = document.createElement("p");
+    normPressure.className = "visitCard__container--normPressure";
+    normPressure.textContent = this.normPressure;
+
+    const prevDiseases = document.createElement("p");
+    prevDiseases.className = "visitCard__container--prevDiseases";
+    prevDiseases.textContent = this.prevDiseases;
+
+    visitCard.append(age);
+    visitCard.append(bodyMass);
+    visitCard.append(normPressure);
+    visitCard.append(prevDiseases);
   }
 }
 
--- js/VisitDentist.js
+++ js/VisitDentist.js
@@ -1,10 +1,17 @@
-import { Visit } from "./Visit.js";
-
+import { visitCard, Visit } from "./Visit.js";
 
 class VisitDentist extends Visit {
   constructor(fio, doctor, purpose, description, priority, lastVisit) {
     super(fio, doctor, purpose, description, priority);
     this.lastVisit = lastVisit;
+  }
+  render() {
+    const lastVisit = document.createElement("p");
+    lastVisit.className = "visitCard__container--lastVisit";
+    lastVisit.textContent = this.lastVisit;
+
+    visitCard.append(lastVisit);
+
   }
 }
 
