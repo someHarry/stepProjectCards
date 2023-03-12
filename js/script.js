021d6002e22098299ea516d32d898c15820804f8
KarinaIvaschenko <karinka2323@gmail.com>
2023-03-12 11:52:35 +0200

    added formVisit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 import {buttonLogin, buttonCreateCard, loginUrl} from "./constants.js";
-import {token, isToken, loginHandler, TEMPORARYcredentials} from "./Login.js";
+import {token, isToken, loginHandler} from "./Login.js";
 import {Requests} from "./Requests.js";
 
 import Modal  from "./Modal.js";
@@ -13,6 +13,6 @@ const formLogin = new FormLogin("knopka", function () {
   // console.log(this)
 });
 const formVisit = new FormVisit('hi',function (){});
-let mymodal = new Modal(modalTemplate, 'Login', formVisit.render(formLogin.renderInput()), 'modal-window');
+let mymodal = new Modal(modalTemplate, 'Login', formLogin.render(formVisit.renderVisit()), 'modal-window');
 
 document.querySelector("body").append(mymodal.render());
