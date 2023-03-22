e6955f132201638353a2789dabcf85d08dd76744
KarinaIvaschenko <karinka2323@gmail.com>
2023-03-22 11:23:27 +0200

    added readme

--- js/createFieldsDentist.js
+++ js/createFieldsDentist.js
@@ -11,9 +11,4 @@ export default function createFieldsDentist (){
 
     fragment.append(labelData, inputData);
     return fragment;
-    // <label htmlFor="start">Start date:</label>
-    //
-    // <input type="date" id="start" name="trip-start"
-    //        value="2018-07-22"
-    //        min="2018-01-01" max="2018-12-31">
 }
