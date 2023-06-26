13c1e1d01a0ea1d3a75b1711554a8beca6b2cc58
HarrySome <21parit@gmail.com>
2023-06-26 21:46:29 +0300

    img folder

--- js/renderPlaceholder.js
+++ js/renderPlaceholder.js
@@ -4,7 +4,7 @@ function renderPlaceholder() {
   visitCardContainer.innerHTML = `<section class="images pt-3" id="placeholder">
                                             <div class="images-container mx-auto " style="width: 25rem;">
 <!--                                                <img src="https://www.englishdom.com/dynamicus/blog-post/000/002/383/1637751298_content_700x455.jpg" class="card-img-top mb-4" alt="">-->
-                                                <img src="https://www.meme-arsenal.com/memes/3e517df66f475a773da6dbb29e812d2e.jpg" class="card-img-top mb-4" alt="meme">
+                                                <img src="/img/placeholder_meme.jpg" class="card-img-top mb-4" alt="meme">
                                                 <p class="images-text text-center fs-3 text-light"> No items have been added</p>
                                             </div>
                                         </section>`;
